c50b2444ab2865df87a8c372d42b7f1a6410691c
Antony <officialantony23@gmail.com>
2023-01-20 15:34:49 +0300

    code complete

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        checkNumber(6);
+    }
+    public static void checkNumber(int number){
+        if(number >0){
+            System.out.println("positive");
+        }
+        else if( number <0){
+            System.out.println("negative");
+        }
+        else {
+            System.out.println("zero");
+        }
+    }
+}
