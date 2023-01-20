801f4e0ae2174d05911cca6dc43b7cc253d9897b
Antony <officialantony23@gmail.com>
2023-01-20 16:22:52 +0300

    positive,negative or zero check

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        checkNumber(6);
+        checkNumber(8);
     }
     public static void checkNumber(int number){
         if(number >0){
